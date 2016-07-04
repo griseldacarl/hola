5f2319701da8d2e77b167e170e67fcf91199067a
griseldacarl <griseldacarl@gmail.com>
2016-07-03 23:10:00 -0700

    I just change the semicolon again

--- hola.go
+++ hola.go
@@ -34,6 +34,6 @@ func editHandler(w http.ResponseWriter, r *http.Request){
       p  =  &Page{Title: title}
    }
   t, _ := template.ParseFiles("static/edit.html");
-  t.Execute(w,p)
+  t.Execute(w,p);
 
 }
